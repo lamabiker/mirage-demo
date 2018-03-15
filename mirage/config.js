acfcda366c4ba0cc9ed4782aadaf6e8caa5b0e4c
Liam Baker <liam.baker@storyful.com>
2018-03-15 15:59:28 +0000

    make json static

--- mirage/config.js
+++ mirage/config.js
@@ -1,8 +1,4 @@
-import { faker } from 'ember-cli-mirage';
-
-function rand(max) {
-  return Math.round(Math.random() * max);
-}
+import { catchphrase, github } from './static/stats';
 
 export default function() {
 
@@ -35,21 +31,6 @@ export default function() {
   this.resource('fe-dev');
 
   this.get('stats/:id', () => {
-
-    const catchphrase = faker.company.bs(5);
-
-    const github = {
-      branches: rand(1000),
-      commits: rand(900000),
-      forks: rand(2000),
-      issues: {
-        created: rand(2000),
-        closed: rand(2000),
-      },
-      repos: rand(200),
-      stars: rand(50)
-    };
-
     return { github, catchphrase };
   })
 }
